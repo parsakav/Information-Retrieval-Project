9dd8857cf019ab7c8f6ce1cdce284cd4337812e6
sanazard <sanaz.ardeshiri18@gmail.com>
2025-01-15 17:25:04 +0330

    Added inverted index as dictionary . the key is Term and value is Doc id's(List of doc id's)

--- main.py
+++ main.py
@@ -3,15 +3,38 @@ import string
 
 print(string.punctuation)
 
-def remove_punctuations(text :str)->str:
 
+def remove_punctuations(text: str) -> str:
     s = [x for x in text if x not in string.punctuation]
     return ''.join(s)
 
-def read_documents(baseUrl: str):
+
+def read_document(document: str) -> str:
+    with open(f'Documents/{document}', "r") as file:
+        return remove_punctuations(file.read().lower())
+
+
+def read_documents() -> dict:
+    d = dict()
     for path in os.listdir('Documents'):
-        with open(f'Documents/{path}', "+r") as file:
-            print(remove_punctuations(file.read().lower()))
+        d[path] = read_document(path)
+    return d;
 
 
-read_documents(baseUrl='Document')
+def inverted_index() -> dict:
+    docs = read_documents()
+    print(docs)
+
+    invertedindex = dict()
+    for x in docs:
+        l = docs[x].split()
+        for z in l:
+            if z in invertedindex:
+                invertedindex[z].add(x)
+            else:
+                invertedindex[z] = {x}
+
+    return invertedindex
+
+
+print(inverted_index())
